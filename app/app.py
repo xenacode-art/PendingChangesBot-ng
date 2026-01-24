c6d668ced588d7afb70f2174f905858521551d6e
Harshita <harshita.21ug1034@iiitranchi.ac.in>
2026-01-24 12:22:43 +0530

    minor fix

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ["IS_TOOLFORGE"] = "true"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reviewer.settings")
 
 app = get_wsgi_application()
